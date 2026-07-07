ca11d1a73f322f041abb3fec030bee6497b5c682
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2026-07-07 14:06:04 +0600

    cors change

--- config/settings/base.py
+++ config/settings/base.py
@@ -186,6 +186,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8080",
     "http://127.0.0.1:9000",
+    "https://vendor-nest.vercel.app",
 ]
 
 # REST Framework Configuration
