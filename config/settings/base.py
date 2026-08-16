db03bd69967dd1712e0cbf166027a7f88606c970
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2026-08-17 01:07:10 +0600

    fix(ci): add sqlite fallback and noinput flag for automated tests

--- config/settings/base.py
+++ config/settings/base.py
@@ -191,7 +191,7 @@ if DATABASE_URL:
             ssl_require=db_ssl
         )
     }
-else:
+elif os.getenv('DB_PASSWORD'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
@@ -203,6 +203,13 @@ else:
             'OPTIONS': {
                 'sslmode': 'require',
             },
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
